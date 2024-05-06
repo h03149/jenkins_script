8cdfb904291948e90d1cc4afda32b22ff3eb9481
h03149 <162279546+h03149@users.noreply.github.com>
2024-05-06 16:48:50 +0900

    Delete vars directory

--- vars/checkUpstreamJobs.groovy
+++
@@ -1,8 +0,0 @@
-def call(upstreamJobs) {
-    def allUpstreamSuccess = upstreamJobs.every { jobName ->
-        def job = Jenkins.instance.getItemByFullName(jobName)
-        def lastBuild = job.getLastBuild()
-        return lastBuild && lastBuild.result == 'SUCCESS'
-    }
-    return allUpstreamSuccess
-}
